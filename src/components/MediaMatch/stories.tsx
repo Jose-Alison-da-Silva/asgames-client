6690cba196b8b42f893eaed51a0cc9f8e548f4cc
Alison Silva <alisonsilva0999@gmail.com>
2024-11-22 09:37:13 -0300

    Add Heading component

---
+++ src/components/MediaMatch/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import MediaMatch from '.'
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch
+} as Meta
+
+export const Default: StoryObj = {}
