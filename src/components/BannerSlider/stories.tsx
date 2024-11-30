7dde881832d6b898bd7cbcd8609c51bbf356b6b1
Alison Silva <alisonsilva0999@gmail.com>
2024-11-30 13:50:32 -0300

    Add BannerSlider component

---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from '@storybook/react'
+import BannerSlider from '.'
+
+const items = [
+  {
+    img: 'https://images.gog-statics.com/45a284386e693f1576b96d98a0023a7905d3956c6f9aa913d3fe5d09a5994bee.png',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Best selling'
+  },
+  {
+    img: 'https://images.gog-statics.com/45a284386e693f1576b96d98a0023a7905d3956c6f9aa913d3fe5d09a5994bee.png',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: 'https://images.gog-statics.com/45a284386e693f1576b96d98a0023a7905d3956c6f9aa913d3fe5d09a5994bee.png',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider,
+  args: {
+    items
+  },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: StoryObj = {
+  decorators: [
+    (Story) => (
+      <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+        <Story />
+      </div>
+    )
+  ]
+}
