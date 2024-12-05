c7bf8c6ff05d14fd661fe2e54e257a24727aef8e
Alison Silva <alisonsilva0999@gmail.com>
2024-12-05 11:56:04 -0300

    Add GameInfo component

---
+++ src/components/GameInfo/stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react'
+import GameInfo, { GameInfoProps } from '.'
+import mockGame from './mock'
+
+export default {
+  title: 'GameInfo',
+  component: GameInfo,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGame
+} as Meta
+
+export const Default: StoryObj<GameInfoProps> = {
+  decorators: [
+    (Story) => (
+      <div style={{ maxWidth: '130rem', margin: '0 auto', padding: '1.6rem' }}>
+        <Story />
+      </div>
+    )
+  ]
+}
